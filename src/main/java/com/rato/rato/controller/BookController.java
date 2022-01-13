4cefdcbbd5cd7b14b13ec9ed6cd67826a69cb679
Axel <axel_nomore@hotmail.com>
2022-01-13 19:36:51 -0400

    fix(estructure): add repository

--- src/main/java/com/rato/rato/controller/BookController.java
+++ src/main/java/com/rato/rato/controller/BookController.java
@@ -1,10 +1,14 @@
 package com.rato.rato.controller;
 
+import com.rato.rato.repository.BookRepository;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
+  
+  private BookRepository bookRepository;
   
 }
