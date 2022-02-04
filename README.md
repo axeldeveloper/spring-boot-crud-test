7deaff22345a4a68beb00da9361cef2ef6c2c477
Axel <axel_nomore@hotmail.com>
2022-02-04 09:55:58 -0400

    fix(book): add book api

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Spring CLI
-  - mvn spring-boot:run
+  - $ ./mvnw packages
+  - $ ./mvnw install
+  - $ ./mvnw clean
+  - $ ./mvnw spring-boot:run
+ 
+
+# Add Maven Plugin to POM.XML
+```xml
+<build>
+   <plugins>
+     <plugin>
+       <groupId>org.springframework.boot</groupId>
+       <artifactId>spring-boot-maven-plugin</artifactId>
+      </plugin>
+    </plugins>
+  </build>
+```
+
+# Build Spring Boot Project with Maven
+  - ./mvnw packages
+  
+or
+
+  - ./mvnw install 
+  - ./mvnw clean install
+
+# Run Spring Boot app using Maven:
+
+    ./mvnw spring-boot:run
--- src/main/java/com/rato/rato/controller/BookController.java
+++ src/main/java/com/rato/rato/controller/BookController.java
@@ -1,14 +1,75 @@
 package com.rato.rato.controller;
 
 import com.rato.rato.repository.BookRepository;
+import com.rato.rato.entity.Book;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
   
-  private BookRepository bookRepository;
+  private BookRepository repository;
+
+  @Autowired
+  public BookController(BookRepository bookRepository){
+    this.repository = bookRepository;
+  }
+  
+  // CRUD methods here
+  @GetMapping
+  public List findAll(){
+    return repository.findAll();
+  }
+
+  @GetMapping(path = {"/{id}"})
+  public ResponseEntity<Book> findById(@PathVariable long id){
+    return repository.findById(id)
+            .map(record -> ResponseEntity.ok().body(record))
+            .orElse(ResponseEntity.notFound().build());
+  }
+
+  @PostMapping
+  public Book create(@RequestBody Book instance){
+      return repository.save(instance);
+  }
+
+  @PutMapping(value="/{id}")
+  public ResponseEntity<Book> update(@PathVariable("id") long id,
+                                        @RequestBody Book instance){
+    return repository.findById(id)
+        .map(record -> {
+            record.setName(instance.getName());
+            record.setChapter(instance.getChapter());
+
+
+            //record.setName(instance.getName());
+            //record.setPhone (contact.getPhone());
+            //record.setPhone(contact.getPhone());
+            Book updated = repository.save(record);
+            return ResponseEntity.ok().body(updated);
+        }).orElse(ResponseEntity.notFound().build());
+  }
+
+  @DeleteMapping(path ={"/{id}"})
+  public ResponseEntity<?> delete(@PathVariable("id") long id) {
+    return repository.findById(id)
+        .map(record -> {
+            repository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }).orElse(ResponseEntity.notFound().build());
+  }
+
   
 }
